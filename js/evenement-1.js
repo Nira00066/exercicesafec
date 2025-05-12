7d8cff8e74d3a34d23472d23f97a47a53db87726
Nira00066 <164557554+Nira00066@users.noreply.github.com>
2025-05-12 15:10:50 +0200

    3/4 exo

--- js/evenement-1.js
+++ js/evenement-1.js
@@ -1,17 +1,48 @@
 function actionHello() {
   alert("bravo, tu as cliqué !");
 }
+// Exo 1
 
+function action() {
+  let btn = document.getElementById("btnaction");
+
+  btn.addEventListener("mouseover", function () {
+    btn.style.transform = "scale(1.2)";
+  });
+  btn.addEventListener("mouseout", function () {
+    btn.style.transform = "scale(1)";
+  });
+}
+// exo 2
 function changeColor() {
   let zone = document.getElementById("paragraphe");
 
   zone.addEventListener("mouseover", function () {
     zone.style.color = "red";
   });
-zone.addEventListener('mouseout', function(){
-  zone.style.color="black";
-})
+  zone.addEventListener("mouseout", function () {
+    zone.style.color = "black";
+  });
 }
+// exo 3
 
-   window.onload = changeColor;
-  //  sert de mettre en jour le chargement
+
+window.onload = function () {
+  changeColor();
+  action();
+  actionClik();
+};
+
+
+// exo 3 et 4  et de faire un btn click avec le compteur et lexo et que le compteur s'arrete a  3
+let count =0;
+
+
+function actionClick() {
+  if (count < 3) {
+    count++;
+    document.getElementById("compteur").textContent = count;
+  } else {
+    alert("T'as déjà cliqué 3 fois, calme-toi 😅");
+  }
+}
